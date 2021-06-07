53808d91dd4e18c7199e1050b273c741f6741791
konstantin <Konstantin_Sinyachevskiy@epam.com>
2021-06-07 13:41:55 +0500

    tests refactoring

--- service/fs.test.ts
+++ service/fs.test.ts
@@ -1,79 +1,82 @@
 import { VirtualFileSystem } from './src/fs/VirtualFileSystem';
 
+describe('Virtual File System', () => {
+    let vfs = null;
 
-test('Check VFS creation', () => {
-    const vfs = new VirtualFileSystem();
-    expect(vfs instanceof VirtualFileSystem ).toBe(true);
-    expect(vfs !== null && vfs !== undefined).toBeTruthy();
+    beforeEach(() => {
+        vfs = new VirtualFileSystem();
+    });
+
+    it('Check VFS creation', () => {
+        expect(vfs instanceof VirtualFileSystem ).toBe(true);
+        expect(vfs !== null && vfs !== undefined).toBeTruthy();
+    });
+
+    it('Check if VFS creates empty list on init', () => {
+        expect(vfs.list()).toStrictEqual([]);
+    });
+
+    it('Check if VFS creates and sorts nodes', () => {
+        vfs.createDirectory('oth');
+        vfs.createDirectory('dir');
+        vfs.createDirectory('dir/sub');
+        expect(vfs.list()).toStrictEqual(['dir', ' sub', 'oth']);
+    });
+
+    it('Check if VFS fails to create nodes with similar names', () => {
+        vfs.createDirectory('dir');
+        expect(() => { vfs.createDirectory('dir') })
+            .toThrow('Directory already exists.');
+    });
+
+    it('Check if VFS deletes node', () => {
+        vfs.createDirectory('dir');
+        vfs.createDirectory('dir/sub');
+        vfs.deleteNode('dir/sub');
+        expect(vfs.list()).toStrictEqual(['dir']);
+    });
+
+    it('Check if VFS fails to delete non-existent node', () => {
+        vfs.createDirectory('dir');
+        expect(() => { vfs.deleteNode('oth'); })
+            .toThrow('Cannot delete oth - oth does not exist');
+    });
+
+    it('Check if VFS fails to delete FROM non-existent node', () => {
+        vfs.createDirectory('dir');
+        expect(() => { vfs.deleteNode('sub/oth'); })
+            .toThrow('Cannot delete sub/oth - sub does not exist');
+    });
+
+    it('Check if VFS moves nodes', () => {
+        vfs.createDirectory('dir');
+        vfs.createDirectory('dir/sub');
+        vfs.createDirectory('dir/sub/under');
+        vfs.createDirectory('oth');
+        vfs.moveNode('dir/sub', 'oth');
+        expect(vfs.list()).toStrictEqual(['dir', 'oth', ' sub', '  under']);
+    });
+
+    it('Check if VFS fails to mode non-existent node', () => {
+        vfs.createDirectory('dir');
+        vfs.createDirectory('dir/sub');
+        vfs.createDirectory('oth');
+        expect(() => { vfs.moveNode('oth/sub', 'dir'); })
+            .toThrow('Cannot find object to move oth/sub');
+    });
+
+    it('Check if VFS fails to move nodes with similar names to the same folder', () => {
+        vfs.createDirectory('dir');
+        vfs.createDirectory('dir/sub');
+        vfs.createDirectory('oth');
+        vfs.createDirectory('oth/sub');
+        expect(() => { vfs.moveNode('oth/sub', 'dir'); })
+            .toThrow('Cannot add node with same name (check existing nodes in current folder).')
+    });
 });
 
-test('Check if VFS creates empty list on init', () => {
-    const vfs = new VirtualFileSystem();
-    expect(vfs.list()).toStrictEqual([]);
-});
 
-test('Check if VFS creates and sorts nodes', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('oth');
-    vfs.createDirectory('dir');
-    vfs.createDirectory('dir/sub');
-    expect(vfs.list()).toStrictEqual(['dir', ' sub', 'oth']);
-});
 
-test('Check if VFS fails to create nodes with similar names', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    expect(() => { vfs.createDirectory('dir') })
-        .toThrow('Directory already exists.');
-});
 
-test('Check if VFS deletes node', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    vfs.createDirectory('dir/sub');
-    vfs.deleteNode('dir/sub');
-    expect(vfs.list()).toStrictEqual(['dir']);
-});
 
-test('Check if VFS fails to delete non-existent node', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    expect(() => { vfs.deleteNode('oth'); })
-        .toThrow('Cannot delete oth - oth does not exist');
-});
 
-test('Check if VFS fails to delete FROM non-existent node', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    expect(() => { vfs.deleteNode('sub/oth'); })
-        .toThrow('Cannot delete sub/oth - sub does not exist');
-});
-
-test('Check if VFS moves nodes', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    vfs.createDirectory('dir/sub');
-    vfs.createDirectory('dir/sub/under');
-    vfs.createDirectory('oth');
-    vfs.moveNode('dir/sub', 'oth');
-    expect(vfs.list()).toStrictEqual(['dir', 'oth', ' sub', '  under']);
-});
-
-test('Check if VFS fails to mode non-existent node', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    vfs.createDirectory('dir/sub');
-    vfs.createDirectory('oth');
-    expect(() => { vfs.moveNode('oth/sub', 'dir'); })
-        .toThrow('Cannot find object to move oth/sub');
-});
-
-test('Check if VFS fails to move nodes with similar names to the same folder', () => {
-    const vfs = new VirtualFileSystem();
-    vfs.createDirectory('dir');
-    vfs.createDirectory('dir/sub');
-    vfs.createDirectory('oth');
-    vfs.createDirectory('oth/sub');
-    expect(() => { vfs.moveNode('oth/sub', 'dir'); })
-        .toThrow('Cannot add node with same name (check existing nodes in current folder).')
-})
